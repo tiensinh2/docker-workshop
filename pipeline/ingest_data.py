f7ca306489b2e01ca1a7830a764f721cf3d206af
tkiet <smg10vp@gmail.com>
2026-05-10 03:36:25 +0000

    add docker pipeline

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+# Read a sample of the data
+prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz', nrows=100)
+
+# Display first rows
+df.head()
+
+# Check data types
+df.dtypes
+
+# Check data shape
+df.shape
+
+
+# In[2]:
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+df = pd.read_csv(
+    prefix + 'yellow_tripdata_2021-01.csv.gz',
+    nrows=100,
+    dtype=dtype,
+    parse_dates=parse_dates
+)
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql+psycopg://root:root@localhost:5432/ny_taxi')
+
+
+# In[5]:
+
+
+print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+
+
+# In[6]:
+
+
+df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+
+
+# In[8]:
+
+
+df_iter = pd.read_csv(
+    prefix + 'yellow_tripdata_2021-01.csv.gz',
+    dtype=dtype,
+    parse_dates=parse_dates,
+    iterator=True,
+    chunksize=100000
+)
+
+
+# In[9]:
+
+
+first = True
+from tqdm.auto import tqdm
+for df_chunk in tqdm(df_iter):
+
+    if first:
+        # Create table schema (no data)
+        df_chunk.head(0).to_sql(
+            name="yellow_taxi_data",
+            con=engine,
+            if_exists="replace"
+        )
+        first = False
+        print("Table created")
+
+    # Insert chunk
+    df_chunk.to_sql(
+        name="yellow_taxi_data",
+        con=engine,
+        if_exists="append"
+    )
+
+    print("Inserted:", len(df_chunk))
+
+
+# In[10]:
+
+
+import click
+
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
+    # Ingestion logic here
+    pass
+
+
+# In[ ]:
+
+
+
+
